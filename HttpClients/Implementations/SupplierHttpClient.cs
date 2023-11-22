8fc9a17b2e4af2ff43eb56c820ea7badc3a6310b
Kajetan Pawlowski <kpawlowski1007@gmail.com>
2023-11-22 21:00:30 +0100

    More auth

--- HttpClients/Implementations/SupplierHttpClient.cs
+++ HttpClients/Implementations/SupplierHttpClient.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Domain.DTO;
 using Domain.Model;
 using HttpClients.Interfaces;
@@ -6,6 +7,14 @@ namespace HttpClients.Implementations;
 
 public class SupplierHttpClient : ISupplierHttpClient
 {
+    private readonly HttpClient client;
+    private readonly IAuthHttpClient authHttpClient;
+
+    public SupplierHttpClient(HttpClient client,  IAuthHttpClient authHttpClient)
+    {
+        this.client = client;
+        this.authHttpClient = authHttpClient;
+    }
     public Task<Supplier> GetSupplierByEmail(string email)
     {
         throw new NotImplementedException();
@@ -21,9 +30,20 @@ public class SupplierHttpClient : ISupplierHttpClient
         throw new NotImplementedException();
     }
 
-    public Task<List<Supplier>> GetSuppliers()
+    public async Task<List<Supplier>> GetSuppliers()
     {
-        throw new NotImplementedException();
+        HttpResponseMessage response = await client.GetAsync("/Supplier");
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(response +"");
+        }
+
+        List<Supplier> suppliers = JsonSerializer.Deserialize<List<Supplier>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return suppliers;
     }
 
     public Task<Survey> ResendSurvey(int surveyId)
