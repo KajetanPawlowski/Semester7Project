b145063808203ec9734715241e4b94befe97728d
Ary2324 <304997@via.dk>
2023-11-22 19:30:36 +0100

    Login

--- Domain/Model/User.cs
+++ Domain/Model/User.cs
@@ -7,5 +7,6 @@ public class User
     public string Mail { get; set; }
     public string Password { get; set; }
     public string FullName { get; set; }
+    //WoltUser, WoltSpecialist, Supplier
     public string Role { get; set; }
 }
---
+++ WebAPI/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.LogicInterface;
+using Domain.DTO;
+using Domain.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace WebAPI;
+
+[ApiController]
+[Route("login")]
+
+public class AuthController : ControllerBase
+{
+    private readonly IUserLogic userLogic;
+    private readonly IConfiguration config;
+
+    public AuthController(IConfiguration config, IUserLogic userLogic)
+    {
+        this.config = config;
+        this.userLogic = userLogic;
+    }
+
+    private List<Claim> GenerateClaims(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, config["Jwt:Subject"]),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+            new Claim(ClaimTypes.Name, user.FullName),
+            new Claim(ClaimTypes.Role, user.Role),
+
+        };
+        return claims.ToList();
+
+    }
+    
+    private string GenerateJwt(User user)
+    {
+        List<Claim> claims = GenerateClaims(user);
+
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+        SigningCredentials signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+        JwtHeader header = new JwtHeader(signIn);
+
+        JwtPayload payload = new JwtPayload(
+            config["Jwt:Issuer"],
+            config["Jwt:Audience"],
+            claims,
+            null,
+            DateTime.UtcNow.AddMinutes(60));
+
+        JwtSecurityToken token = new JwtSecurityToken(header, payload);
+
+        string serializedToken = new JwtSecurityTokenHandler().WriteToken(token);
+        return serializedToken;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> Login([FromBody] UserLoginDTO userLoginDto)
+    {
+        try
+        {
+            User user = await userLogic.ValidateUserAsync(userLoginDto);
+            string token = GenerateJwt(user);
+
+            return Ok(token);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    
+}
