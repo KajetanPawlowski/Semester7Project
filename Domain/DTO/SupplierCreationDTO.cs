66b2a51a39078ffa38baebf465c2bd736e6ffd31
Kajetan Pawlowski <kpawlowski1007@gmail.com>
2023-11-23 12:04:29 +0100

    Add Supliaer Page

--- Domain/DTO/SupplierCreationDTO.cs
+++ Domain/DTO/SupplierCreationDTO.cs
@@ -2,10 +2,10 @@ namespace Domain.DTO;
 
 public class SupplierCreationDTO
 {
-    public int RepresentativeId { get; init; }
-    public string SupplierName { get; init; }
-    public string Country { get; init; }
-    public int HeadCount { get; init; }
-    public List<string> SuppliedProducts { get; init; }
+    public int RepresentativeId { get; set; }
+    public string SupplierName { get; set; }
+    public string Country { get; set; }
+    public int HeadCount { get; set; }
+    public List<string>? SuppliedProducts { get; set; }
     
 }
