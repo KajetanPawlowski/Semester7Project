0f15eb244afc20fc5f5accf205cb25bc8bd430cd
Kajetan Pawlowski <kpawlowski1007@gmail.com>
2023-12-02 11:54:04 +0100

    without styles

--- Domain/DTO/CreateQuestionDTO.cs
+++ Domain/DTO/CreateQuestionDTO.cs
@@ -7,6 +7,9 @@ public class CreateQuestionDTO
     public string Body { get; set; }
     public List<string> AllAnswers { get; set; }
     public RiskCategory RiskCategory { get; set; }
-    public Risk? RelatedRisk { get; set; }
+    public Risk RelatedRisk { get; set; }
+    
+    public string CCode { get; set; }
+    public int CompanySize { get; set; }
     
 }
